4b5756a5d28093a7ce0c8b8c590a4b0bb2a8cf62
petcupaula <petcu.paula@gmail.com>
2016-10-16 20:15:56 +0200

    Added sketch for sending scale values to Adafruit.IO service

---
+++ Scale_HuzzahFeatherAdafruitIoMQTT/Scale_HuzzahFeatherAdafruitIoMQTT.ino
@@ -0,0 +1,162 @@
+/***************************************************
+  This code sends the value read from the scale to Adafruit.IO.
+  
+  An account with Adafruit.IO is required in order to make this sketch work.
+
+  Don't forget to fill in the wifi connection info and your adafruit.io info below!
+  
+  This code is based on the following example:
+  
+  Adafruit MQTT Library ESP8266 Example
+
+  Must use ESP8266 Arduino from:
+    https://github.com/esp8266/Arduino
+
+  Works great with Adafruit's Huzzah ESP board & Feather
+  ----> https://www.adafruit.com/product/2471
+  ----> https://www.adafruit.com/products/2821
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+
+  Written by Tony DiCola for Adafruit Industries.
+  MIT license, all text above must be included in any redistribution
+ ****************************************************/
+ 
+#include <ESP8266WiFi.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
+#include "HX711.h"
+
+/************************* Scale ***********************************************/
+
+#define calibration_factor -2007.0 //Use value from calibration sketch
+
+// Use the following for Adafruit Huzzah: 
+#define DOUT  4
+#define CLK  5
+
+HX711 scale(DOUT, CLK);
+
+/************************* WiFi Access Point *********************************/
+
+#define WLAN_SSID       ""
+#define WLAN_PASS       ""
+
+/************************* Adafruit.io Setup *********************************/
+
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  8883  //1883                   // use 8883 for SSL
+#define AIO_USERNAME    ""
+#define AIO_KEY         ""
+
+/************ Global State (you don't need to change this!) ******************/
+
+// Create an ESP8266 WiFiClient class to connect to the MQTT server.
+//WiFiClient client;
+// or... use WiFiFlientSecure for SSL
+WiFiClientSecure client;
+
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+
+/****************************** Feeds ***************************************/
+
+// Setup a feed for publishing.
+// Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
+Adafruit_MQTT_Publish scaleFeed = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/scale");
+
+/*************************** Sketch Code ************************************/
+
+// Bug workaround for Arduino 1.6.6, it seems to need a function declaration
+// for some reason (only affects ESP8266, likely an arduino-builder bug).
+void MQTT_connect();
+
+void setup() {
+  Serial.begin(115200);
+
+  Serial.println(F("Adafruit MQTT demo"));
+
+  // Connect to WiFi access point.
+  Serial.println(); Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WLAN_SSID);
+
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.println("WiFi connected");
+  Serial.println("IP address: "); Serial.println(WiFi.localIP());
+
+
+  /************************* SCALE SETUP ***********************************************/
+  scale.set_scale(calibration_factor); //This value is obtained by using the SparkFun_HX711_Calibration sketch
+  scale.tare();  //Assuming there is no weight on the scale at start up, reset the scale to 0
+  
+}
+
+uint32_t x=0;
+
+void loop() {
+  // Ensure the connection to the MQTT server is alive (this will make the first
+  // connection and automatically reconnect when disconnected).  See the MQTT_connect
+  // function definition further below.
+  MQTT_connect();
+
+  /************************* SEND SCALE VALUE ***********************************************/
+
+  float scaleValue = scale.get_units();
+  
+  Serial.print(F("\nSending scale val "));
+  Serial.print(scaleValue,1);
+  Serial.print(" g"); //Use same units as those used when calibrating
+  Serial.println();
+  if (! scaleFeed.publish(scaleValue)) {
+    Serial.println(F("Failed"));
+  } else {
+    Serial.println(F("OK!"));
+  }
+
+  delay(10000);
+
+  // ping the server to keep the mqtt connection alive
+  // NOT required if you are publishing once every KEEPALIVE seconds
+  /*
+  if(! mqtt.ping()) {
+    mqtt.disconnect();
+  }
+  */
+}
+
+// Function to connect and reconnect as necessary to the MQTT server.
+// Should be called in the loop function and it will take care if connecting.
+void MQTT_connect() {
+  int8_t ret;
+
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+
+  Serial.print("Connecting to MQTT... ");
+
+  uint8_t retries = 3;
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+       Serial.println(mqtt.connectErrorString(ret));
+       Serial.println("Retrying MQTT connection in 5 seconds...");
+       mqtt.disconnect();
+       delay(5000);  // wait 5 seconds
+       retries--;
+       if (retries == 0) {
+         // basically die and wait for WDT to reset me
+         while (1);
+       }
+  }
+  Serial.println("MQTT Connected!");
+}
